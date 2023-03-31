a4f0cadc09080021207fcfbeacf275ca703cd022
Shoaib Ali <shoaibali26021999@gmail.com>
2023-04-01 02:02:44 +0530

    8th question added

---
+++ Pattern/Eighth.java
@@ -0,0 +1,23 @@
+public class Eighth {
+    public static void main(String[] args) {
+        
+       int n=5;
+
+       for(int i=1;i<=5;i++)
+       {
+
+        for(int j=1;j<=n;j++)
+        {
+            if(j==i||j==n+1-i)
+                System.out.print("* ");
+            else  
+            System.out.print("  ");
+        }
+
+        System.out.println();
+
+       }
+
+    }
+    
+}
